8a26c464108a9e6a6c1b372f9a67dcd70b69c253
mahdiyar <a.mahdiyar7@live.com>
2020-03-13 22:50:11 +0330

    bg added

--- night_lights/index.js
+++ night_lights/index.js
@@ -1,7 +1,12 @@
+const X_AXIS = 1
+const Y_AXIS = 2
 
-
-let bgcolor = "#101"
+let bgcolor = "#0a0026"
 let fgcolor;
+let b1
+let b2
+
+
 function randomint(min, max) {
     return Math.floor(Math.random() * max) + min
 }
@@ -12,11 +17,11 @@ function radialGradient(x, y, w, h, inner, outer) {
         const colour = lerpColor(inner, outer, step);
         fill(colour);
         ellipse(x, y, step * w, step * h);
-        
     }
 
     strokeWeight(3)
     stroke("#fff")
+    // image(bluebeam, x,y,50,100 );
     point(x, y)
 }
 
@@ -57,27 +62,43 @@ class FlyingBox {
             fgcolor[this.color][0],
             fgcolor[this.color][1],
         )
-
         pop()
+        // console.log(flyboxArry[0].y)
 
+        if (this.y < -10) {
+            this.remove()
+        }
+
+    }
+    remove() {
+        flyboxArry.splice(flyboxArry.indexOf(this), 1)
+        console.log(flyboxArry.length)
     }
 }
 
+function preload() {
+    // window.bluebeam = loadImage('./beam.svg');
+}
 
 function setup() {
+    angleMode(DEGREES);
     frameRate(24)
-    createCanvas(innerWidth, innerHeight)
+    b1 = color(48,60,184);
+    b2 = color(1,17,28);
+    
+   var canvas =  createCanvas(innerWidth, innerHeight)
+
     fgcolor = [
         [color(249, 70, 101, 0.1), color(118, 25, 49, 1.9)],
         [color(41, 54, 195, 0.9), color(9, 22, 112, 1.9)],
         [color(175, 61, 255, 2), color(175, 61, 255, 0.2)],
     ];
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < 70; i++) {
         setTimeout(() => {
 
             let wAndH = random(10, 40)
             let fb = new FlyingBox(random(0, width),
-                random(height, height - 200),
+                random(height, height - 550),
                 wAndH,
                 wAndH,
                 (Math.random() * 1.5).toFixed(2),
@@ -89,18 +110,20 @@ function setup() {
 }
 
 function draw() {
-    background(bgcolor);
+    clear()
+    background(`rgba(1,1,1,0)`);
+    // background(0)
+    // setGradient(0, 0,  height,width , b2, b1, X_AXIS);
     flyboxArry.forEach(element => {
-
         element.draw();
     });
-
+    
     push();
     textSize(15)
     textAlign(LEFT, BOTTOM);
     fill(120)
     text("created by mahdiyar anari", 0, height - 7);
     pop();
-
+    
 }
 
