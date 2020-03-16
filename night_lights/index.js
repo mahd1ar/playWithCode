ac8eff785bcc0a97a2c125de9ffca1e2597f251d
mahdiyar <a.mahdiyar7@live.com>
2020-03-16 19:46:56 +0330

    blah

--- night_lights/index.js
+++ night_lights/index.js
@@ -3,26 +3,24 @@ const Y_AXIS = 2
 
 let bgcolor = "#0a0026"
 let fgcolor;
-let b1
-let b2
+let b1, b2, graphic
 
 
 function randomint(min, max) {
     return Math.floor(Math.random() * max) + min
 }
 function radialGradient(x, y, w, h, inner, outer) {
-    noStroke();
-    for (let i = Math.max(w, h); i > 0; i--) {
-        const step = i / Math.max(w, h);
-        const colour = lerpColor(inner, outer, step);
-        fill(colour);
-        ellipse(x, y, step * w, step * h);
-    }
+    // graphic.noStroke();
+    // for (let i = Math.max(w, h); i > 0; i--) {
+    //     const step = i / Math.max(w, h);
+    //     const colour = graphic.lerpColor(inner, outer, step);
+    //     graphic.fill(colour);
+    //     graphic.ellipse(x, y, step * w, step * h);
+    // }
 
-    strokeWeight(3)
-    stroke("#fff")
-    // image(bluebeam, x,y,50,100 );
-    point(x, y)
+    // strokeWeight(map(w, 10, 100, 2, 8))
+    // graphic.stroke("#fff")
+    graphic.point(x, y)
 }
 
 flyboxArry = [];
@@ -83,20 +81,20 @@ function preload() {
 function setup() {
     angleMode(DEGREES);
     frameRate(24)
-    b1 = color(48,60,184);
-    b2 = color(1,17,28);
-    
-   var canvas =  createCanvas(innerWidth, innerHeight)
+    b1 = color(48, 60, 184);
+    b2 = color(1, 17, 28);
 
+    var canvas = createCanvas(innerWidth, innerHeight)
+    graphic = createGraphics(innerWidth, innerHeight)
     fgcolor = [
-        [color(249, 70, 101, 0.1), color(118, 25, 49, 1.9)],
-        [color(41, 54, 195, 0.9), color(9, 22, 112, 1.9)],
-        [color(175, 61, 255, 2), color(175, 61, 255, 0.2)],
+        [color(249, 70, 101, 0.5), color(118, 25, 49, 0)],
+        [color(41, 54, 195, 0.7), color(9, 22, 112, 0)],
+        [color(58, 152, 255, 1), color(46, 122, 255, 0.2)],
     ];
     for (let i = 0; i < 70; i++) {
         setTimeout(() => {
 
-            let wAndH = random(10, 40)
+            let wAndH = random(10, 100)
             let fb = new FlyingBox(random(0, width),
                 random(height, height - 550),
                 wAndH,
@@ -110,20 +108,23 @@ function setup() {
 }
 
 function draw() {
-    clear()
+
     background(`rgba(1,1,1,0)`);
     // background(0)
     // setGradient(0, 0,  height,width , b2, b1, X_AXIS);
     flyboxArry.forEach(element => {
         element.draw();
     });
-    
+    graphic.fill(120)
+    graphic.rect(10, 10, 10, 10)
+    image(graphic, innerWidth, innerHeight)
     push();
     textSize(15)
     textAlign(LEFT, BOTTOM);
     fill(120)
     text("created by mahdiyar anari", 0, height - 7);
     pop();
-    
+
+
 }
 
